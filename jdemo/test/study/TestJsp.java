54cbfddcf0760eebd8823ad20d7ae4ce5742aeae
taowei <taowei20061122@163.com>
2015-01-26 16:03:01 +0800

    gitignore

--- jdemo/test/study/TestJsp.java
+++ jdemo/test/study/TestJsp.java
@@ -5,6 +5,9 @@
  */
 package study;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 
@@ -16,7 +19,7 @@ public class TestJsp {
 		try {
 			System.out.println("try 语句块执行 start");
 			flag = true;
-			if (flag) {
+			if (flag) {//直接返回 finally始终是要执行
 				return;
 			}
 			System.out.println("try 语句块执行 end");
@@ -24,6 +27,44 @@ public class TestJsp {
 			// TODO: handle exception
 		} finally {
 			System.out.println("finally释放资源");
+			String bString = "true";
+			Boolean boolean1 = Boolean.valueOf(bString);
+			Boolean boolean2 = Boolean.parseBoolean(bString);
+			System.out.println(boolean1 + " " + boolean2);
+			
+//			另一方面，下面的代码不能工作：
+
+			List<Integer> li = new ArrayList<Integer>();
+			li.add(new Integer(42));
+			List<?> lu = li;
+//			lu.add(new Integer(43)); // error
+			Number number = new Number() {
+				
+				@Override
+				public long longValue() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+				
+				@Override
+				public int intValue() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+				
+				@Override
+				public float floatValue() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+				
+				@Override
+				public double doubleValue() {
+					// TODO Auto-generated method stub
+					return 0;
+				}
+			};
 		}
+		
 	}
 }
---
+++ jdemo/test/study/regex/RegExTest.java
@@ -0,0 +1,14 @@
+/**
+ * @author taowei20061122@163.com 
+ * @createtime 2015年1月26日
+ * @version V1.0
+ */
+package study.regex;
+
+public class RegExTest {
+
+	public static void main(String[] args) {
+
+	}
+
+}
