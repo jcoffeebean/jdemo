dea02860e8775c9e8dabe146e241167ff5c0b23f
taowei <taowei20061122@163.com>
2015-01-26 19:09:23 +0800

    regex study

--- jdemo/test/study/regex/RegexMatches.java
+++ jdemo/test/study/regex/RegexMatches.java
@@ -16,6 +16,8 @@ public class RegexMatches {
 	private static Matcher matcher;
 
 	public static void main(String args[]) {
+		// matches 和lookingAt
+		// 方法都用来尝试匹配一个输入序列模式。它们的不同是matcher要求整个序列都匹配，而lookingAt 不要求
 		pattern = Pattern.compile(REGEX);
 		matcher = pattern.matcher(INPUT);
 
@@ -24,6 +26,35 @@ public class RegexMatches {
 
 		System.out.println("lookingAt(): " + matcher.lookingAt());
 		System.out.println("matches(): " + matcher.matches());
+
+		// replaceFirst 和replaceAll 方法用来替换匹配正则表达式的文本。不同的是，replaceFirst
+		// 替换首次匹配，replaceAll 替换所有匹配
+		String regex = "dog";
+		String input = "The dog says meow. " + "All dogs say meow.";
+		String replace = "cat";
+
+		Pattern p = Pattern.compile(regex);
+		// get a matcher object
+		Matcher m = p.matcher(input);
+		input = m.replaceAll(replace);
+		System.out.println(input);
+		input = m.replaceFirst(replace);
+		System.out.println(input);
+
+		// Matcher 类也提供了appendReplacement 和appendTail 方法用于文本替换
+		regex = "a*b";
+		input = "aabfooaabfooabfoob";
+		replace = "-";
+
+		p = Pattern.compile(regex);
+		// 获取 matcher 对象
+		m = p.matcher(input);
+		StringBuffer sb = new StringBuffer();
+		while (m.find()) {
+			m.appendReplacement(sb, replace);
+		}
+		m.appendTail(sb);
+		System.out.println(sb.toString());
 	}
 
 }
